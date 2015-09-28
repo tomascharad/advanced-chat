1e74c7e56efcf6abb61626592e1abbe89fec5088
Tomás Charad <tomas.chrad@gmail.com>
2015-09-28 09:26:21 -0300

    Cleaning code

--- server.js
+++ server.js
@@ -49,14 +49,6 @@ app.engine('html', require('ejs').renderFile);
 // }
 
 app.get('/', function(req, res) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	// Request methods you wish to allow
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-	// Request headers you wish to allow
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-	// Set to true if you need the website to include cookies in the requests sent
-	// to the API (e.g. in case you use sessions)
-	res.setHeader('Access-Control-Allow-Credentials', true);
   res.render('index.html');
 });
 
